0c63d392537374872bff6a0fde88a1ab6b112ba5
Salo Andrew <comp9@Mac-mini-comp9.local>
2015-03-18 15:33:50 +0200

    alpha
    Brake Down

---
+++ Break Down/Assets/Scripts/Grid.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+	
+	public GameObject sprite;
+	float cellSize = 10f;
+	
+	static public GameObject [,] grid = new GameObject [6,10];
+	
+	void Start () 
+	{
+		//x=Mathf.Abs (-1.1f);
+		float xPos = -30f;
+		for(int x = 0; x < 6; x++)
+		{
+			float yPos = -15f;
+			for(int y = 0; y < 10; y++)
+			{
+				GameObject spriteGrid = (GameObject)Instantiate(sprite);
+				spriteGrid.transform.position = new Vector3(spriteGrid.transform.position.x + xPos, spriteGrid.transform.position.y + yPos, spriteGrid.transform.position.z);
+				spriteGrid.transform.name = "Square" + x + " " +y;
+				grid[x,y] = spriteGrid;
+				//Debug.Log (xPos + "xPos" + yPos + "yPos");
+				
+				yPos += cellSize;
+				
+			}
+			xPos += cellSize;
+			
+		}
+		
+	}
+	
+	void Update () 
+	{
+	}
+	
+}
---
+++ Break Down/Assets/Scripts/GridControl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridControl : MonoBehaviour {
+	
+	public Color[] colorArr = new Color[5];
+	GameObject work;
+	
+	void OnMouseDown (){
+		string s = gameObject.name;
+		int x = int.Parse(s[6].ToString());
+		int y = int.Parse(s[8].ToString());
+		for (int i = x-1; i < x+2; i++) {
+			for (int j = y-1; j < y+2; j++) {
+				if ((i >= 0 && i<6) && (j >= 0 && j <10)) {
+					if (Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[i,j].GetComponent<SpriteRenderer> ().color) {
+						//if (Mathf.Abs(i) != Mathf.Abs(j)) {
+						Grid.grid[i,j].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+						if (Grid.grid[i,j].GetComponent<SpriteRenderer> ().color == new Color (0f, 0f, 0f, 0.255f)) {
+
+							work = Grid.grid[x, y];
+							Grid.grid[x, y] = Grid.grid[x, 10];
+							Grid.grid[x, 10] = work;
+
+
+						}
+							//Debug.Log(x + " x " + y + " y " + i + " i " + j + " j ");
+						//}		
+					}
+				}
+			}
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+		int colorRand = Random.Range (0, 5);
+		GetComponent<SpriteRenderer> ().color = colorArr[colorRand];
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
