843175f42dd81b4cf65ddb137abe949f3f23f959
yuraKh <yurakhodanitcky@gmail.com>
2015-03-18 22:54:47 +0200

    v 0.0.0.1a
    Grid work

--- Break Down/Assets/Scripts/Grid.cs
+++ Break Down/Assets/Scripts/Grid.cs
@@ -6,20 +6,20 @@ public class Grid : MonoBehaviour {
 	public GameObject sprite;
 	float cellSize = 10f;
 	
-	static public GameObject [,] grid = new GameObject [6,10];
+	static public GameObject [,] grid = new GameObject [10,20];
 	
 	void Start () 
 	{
 		//x=Mathf.Abs (-1.1f);
-		float xPos = -30f;
-		for(int x = 0; x < 6; x++)
+		float xPos = 0f;
+		for(int x = 0; x < 10; x++)
 		{
-			float yPos = -15f;
-			for(int y = 0; y < 10; y++)
+			float yPos = 0f;
+			for(int y = 0; y < 20; y++)
 			{
 				GameObject spriteGrid = (GameObject)Instantiate(sprite);
 				spriteGrid.transform.position = new Vector3(spriteGrid.transform.position.x + xPos, spriteGrid.transform.position.y + yPos, spriteGrid.transform.position.z);
-				spriteGrid.transform.name = "Square" + x + " " +y;
+				spriteGrid.transform.name = "Square" + string.Format ("{0:D2}", x) + " " + string.Format ("{0:D2}", y);
 				grid[x,y] = spriteGrid;
 				//Debug.Log (xPos + "xPos" + yPos + "yPos");
 				
--- Break Down/Assets/Scripts/GridControl.cs
+++ Break Down/Assets/Scripts/GridControl.cs
@@ -4,27 +4,63 @@ using System.Collections;
 public class GridControl : MonoBehaviour {
 	
 	public Color[] colorArr = new Color[5];
-	GameObject work;
 	
 	void OnMouseDown (){
 		string s = gameObject.name;
-		int x = int.Parse(s[6].ToString());
-		int y = int.Parse(s[8].ToString());
-		for (int i = x-1; i < x+2; i++) {
-			for (int j = y-1; j < y+2; j++) {
-				if ((i >= 0 && i<6) && (j >= 0 && j <10)) {
-					if (Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[i,j].GetComponent<SpriteRenderer> ().color) {
-						//if (Mathf.Abs(i) != Mathf.Abs(j)) {
-						Grid.grid[i,j].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
-						if (Grid.grid[i,j].GetComponent<SpriteRenderer> ().color == new Color (0f, 0f, 0f, 0.255f)) {
+		int x = int.Parse(s.Substring(6,2));
+		int y = int.Parse(s.Substring(9,2));
+		//GameObject work;
+		/*if ((x >= 0 && x<10) && (y >= 0 && y <20)) {
+				if (Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[x+1 ,y].GetComponent<SpriteRenderer> ().color)
+				{
+					Grid.grid[x,y].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
 
-							work = Grid.grid[x, y];
-							Grid.grid[x, y] = Grid.grid[x, 10];
-							Grid.grid[x, 10] = work;
-
-
+					if (Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[x-1 ,y].GetComponent<SpriteRenderer> ().color)
+					{
+						Grid.grid[x-1,y].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+						
+						if (Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[x ,y+1].GetComponent<SpriteRenderer> ().color)
+						{
+							Grid.grid[x,y+1].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+							
+							if (Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[x ,y-1].GetComponent<SpriteRenderer> ().color)
+							{
+								Grid.grid[x,y-1].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+							}
 						}
-							//Debug.Log(x + " x " + y + " y " + i + " i " + j + " j ");
+					}
+				}
+			}*/
+		Debug.Log("x = " + x +  "; y = " + y + "\n" +
+		          "(x+1) = " + (x + 1) + "; (y+1) = " + (y+1) +
+		          "|| (x-1) = " + (x - 1) + "; (y-1) = " + (y-1));
+	
+		for (int i = x-1; i < x+2; i++) 
+		{
+			for (int j = y-1; j < y+2; j++) 
+			{
+
+				if ((i >= 0 && i<10) && (j >= 0 && j <20)) 
+				{
+					if (Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[i,j].GetComponent<SpriteRenderer> ().color) 
+					{
+						//if (Mathf.Abs(i) == Mathf.Abs(j)) 
+						//{
+							//break;
+						/*}
+						else
+						{*/
+							Grid.grid[i,j].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+							/*if (Grid.grid[i,j].GetComponent<SpriteRenderer> ().color == new Color (0f, 0f, 0f, 0.255f))
+							{
+
+								work = Grid.grid[x, y];
+								Grid.grid[x, y] = Grid.grid[x, 10];
+								Grid.grid[x, 10] = work;
+
+
+							}*/
+
 						//}		
 					}
 				}
