304ba0bcc1e81da7c0b75568588fe9d2c37035a1
Юрій <comp9@Mac-mini-comp9.local>
2015-03-20 13:44:58 +0200

    00000

--- Break Down/Assets/Dash.cs
+++ Break Down/Assets/Dash.cs
@@ -124,3 +124,161 @@ public class GridControl : MonoBehaviour {
 		
 	}
 }*/
+
+
+/*(j - 1 > 0)&&(Grid.grid [x, y].GetComponent<SpriteRenderer> ().color == Grid.grid [x, j - 1].GetComponent<SpriteRenderer> ().color) |
+		    (i + 1 > 10)&&(Grid.grid [x, y].GetComponent<SpriteRenderer> ().color == Grid.grid [i + 1, y].GetComponent<SpriteRenderer> ().color) |
+		    (i - 1 > 0)&&(Grid.grid [x, y].GetComponent<SpriteRenderer> ().color == Grid.grid [i - 1, y].GetComponent<SpriteRenderer> ().color)) {
+
+
+			while (Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[x, j + 1].GetComponent<SpriteRenderer> ().color) {
+				Grid.grid [x, j + 1].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+				j++;
+				gridCountY += 1;
+			} 
+
+
+			j = y;
+
+			while (Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[x, j - 1].GetComponent<SpriteRenderer> ().color) {				
+				Grid.grid [x, j - 1].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+				j--;
+				gridCountY += 1;
+			} 
+			while (Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[i + 1, y].GetComponent<SpriteRenderer> ().color) {					
+				Grid.grid [i + 1, y].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+				i++;
+				gridCountXplus += 1;
+			}
+			i = x;
+
+			while (Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[i - 1, y].GetComponent<SpriteRenderer> ().color) {					
+				Grid.grid [i - 1, y].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+				i--;
+				gridCountXminus += 1;
+			}
+
+			Grid.grid [x, y].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);*/
+
+/*for (int c = x + 1 ; c < gridCountXplus; c++) {
+				
+				for (int a = y; a < 19; a++) {
+					work = Grid.grid [c, a].GetComponent<SpriteRenderer> ().color;
+					Grid.grid [c, a].GetComponent<SpriteRenderer> ().color = Grid.grid [c, a + 1].GetComponent<SpriteRenderer> ().color;
+					Grid.grid [c, a + 1].GetComponent<SpriteRenderer> ().color = work;
+				}
+			}
+			
+			for (int n = x - 1 ; n > gridCountXminus; n--) {
+				
+				for (int m = y; m < 19; m++) {
+					work = Grid.grid [n, m].GetComponent<SpriteRenderer> ().color;
+					Grid.grid [n, m].GetComponent<SpriteRenderer> ().color = Grid.grid [n, m + 1].GetComponent<SpriteRenderer> ().color;
+					Grid.grid [n, m + 1].GetComponent<SpriteRenderer> ().color = work;
+				}
+			}
+
+			for (int q = 0; q < gridCountY; q++) {
+			
+				for (int b = j; b < 19; b++) {
+					work = Grid.grid [x, b].GetComponent<SpriteRenderer> ().color;
+					Grid.grid [x, b].GetComponent<SpriteRenderer> ().color = Grid.grid [x, b + 1].GetComponent<SpriteRenderer> ().color;
+					Grid.grid [x, b + 1].GetComponent<SpriteRenderer> ().color = work;
+				}
+			}*/
+
+/*void Direction (int xCor, int yCor) {
+
+		int a = xCor;
+		int b = yCor;
+
+
+		if ((yCor + 1 < 20) && (Grid.grid [xCor, yCor].GetComponent<SpriteRenderer> ().color == Grid.grid [xCor, yCor + 1].GetComponent<SpriteRenderer> ().color)) {
+
+			while (b + 1 < 20 && Grid.grid[xCor, yCor].GetComponent<SpriteRenderer> ().color == Grid.grid[xCor, b + 1].GetComponent<SpriteRenderer> ().color) {
+
+				Grid.grid [xCor, b + 1].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+				b++;
+				Direction (xCor, b +1);
+			}
+		}
+
+		b = yCor;
+			
+		if ((yCor > 0) && (Grid.grid [xCor, yCor].GetComponent<SpriteRenderer> ().color == Grid.grid [xCor, yCor - 1].GetComponent<SpriteRenderer> ().color)) {
+				
+			while (b -1 >= 0 &&  Grid.grid[xCor, yCor].GetComponent<SpriteRenderer> ().color == Grid.grid[xCor, b - 1].GetComponent<SpriteRenderer> ().color) {	
+
+				Grid.grid [xCor, b - 1].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+				b--;
+				Direction (xCor, b - 1);
+			} 
+		}
+			
+			
+		if ((xCor + 1 < 10) && (Grid.grid [xCor, yCor].GetComponent<SpriteRenderer> ().color == Grid.grid [xCor + 1, yCor].GetComponent<SpriteRenderer> ().color)) {
+
+			while (a + 1 < 10 && Grid.grid[xCor, yCor].GetComponent<SpriteRenderer> ().color == Grid.grid[a + 1, yCor].GetComponent<SpriteRenderer> ().color) {
+
+				Grid.grid [a + 1, yCor].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+				a++;
+				Direction (a + 1, yCor);
+			}
+		}
+			
+		a = xCor;
+			
+		if ((xCor > 0) && (Grid.grid [xCor, yCor].GetComponent<SpriteRenderer> ().color == Grid.grid [xCor - 1, yCor].GetComponent<SpriteRenderer> ().color)) {
+
+			while (a - 1 >= 0 && Grid.grid[xCor, yCor].GetComponent<SpriteRenderer> ().color == Grid.grid[a - 1, yCor].GetComponent<SpriteRenderer> ().color) {	
+
+				Grid.grid [a - 1, yCor].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+				a--;
+				Direction (a - 1, yCor);
+			}
+		}
+	}*/
+/*if ((y + 1 < 20) && (Grid.grid [x, y].GetComponent<SpriteRenderer> ().color == Grid.grid [x, y + 1].GetComponent<SpriteRenderer> ().color)) {
+
+			while (j + 1 < 20 && Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[x, j + 1].GetComponent<SpriteRenderer> ().color) {
+				Direction (x, j + 1);
+				Grid.grid [x, j + 1].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+				j++;
+			}
+		}
+
+		j = y;
+
+		if ((y > 0) && (Grid.grid [x, y].GetComponent<SpriteRenderer> ().color == Grid.grid [x, y - 1].GetComponent<SpriteRenderer> ().color)) {
+
+			while (j -1 >= 0 &&  Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[x, j - 1].GetComponent<SpriteRenderer> ().color) {				
+				Direction (x, j - 1);		
+				Grid.grid [x, j - 1].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+				j--;
+			}
+		}
+
+		if ((x + 1 < 10) && (Grid.grid [x, y].GetComponent<SpriteRenderer> ().color == Grid.grid [x + 1, y].GetComponent<SpriteRenderer> ().color)) {
+
+			while (i + 1 < 10 && Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[i + 1, y].GetComponent<SpriteRenderer> ().color) {					
+				Direction (i + 1, y);	
+				Grid.grid [i + 1, y].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+				i++;
+			}
+		}
+
+		i = x;
+
+		if ((x > 0) && (Grid.grid [x, y].GetComponent<SpriteRenderer> ().color == Grid.grid [x - 1, y].GetComponent<SpriteRenderer> ().color)) {
+
+			while (i - 1 >= 0 && Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[i - 1, y].GetComponent<SpriteRenderer> ().color) {					
+				Direction (i - 1, y);		
+				Grid.grid [i - 1, y].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
+				i--;
+			}
+		}
+	
+		Grid.grid [x, y].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);*/
+
+
+
--- Break Down/Assets/Scripts/GridControl.cs
+++ Break Down/Assets/Scripts/GridControl.cs
@@ -5,96 +5,83 @@ public class GridControl : MonoBehaviour {
 	
 	public Color[] colorArr = new Color[5];
 
+	Color Fon;
+	
+	void Direction (int a, int b){
+
+		Color work;
+		if (a + 1 < 10 && Fon == Grid.grid [a + 1, b].GetComponent<SpriteRenderer> ().color) {
+			Grid.grid [a + 1, b].GetComponent<SpriteRenderer> ().color = Color.clear;
+
+			/*for (int i = b; i < 19; i++) {
+				work = Grid.grid [a + 1, i].GetComponent<SpriteRenderer> ().color;
+				Grid.grid [a + 1, i].GetComponent<SpriteRenderer> ().color = Grid.grid [a + 1, i + 1].GetComponent<SpriteRenderer> ().color;
+				Grid.grid [a + 1, i + 1].GetComponent<SpriteRenderer> ().color = work;
+			}*/
+			Direction (a + 1, b);
+		}
+		if (a > 0 && Fon == Grid.grid [a - 1, b].GetComponent<SpriteRenderer> ().color) {
+			Grid.grid [a - 1, b].GetComponent<SpriteRenderer> ().color = Color.clear;
+
+			/*for (int i = b; i < 19; i++) {
+				work = Grid.grid [a - 1, i].GetComponent<SpriteRenderer> ().color;
+				Grid.grid [a - 1, i].GetComponent<SpriteRenderer> ().color = Grid.grid [a - 1, i + 1].GetComponent<SpriteRenderer> ().color;
+				Grid.grid [a - 1, i + 1].GetComponent<SpriteRenderer> ().color = work;
+			}*/
+			Direction (a - 1, b);
+		}
+		if (b + 1 < 20 && Fon == Grid.grid [a, b + 1].GetComponent<SpriteRenderer> ().color) {
+			Grid.grid [a, b + 1].GetComponent<SpriteRenderer> ().color = Color.clear;
+		
+			/*for (int i = b + 1; i < 19; i++) {
+				work = Grid.grid [a, i].GetComponent<SpriteRenderer> ().color;
+				Grid.grid [a , i].GetComponent<SpriteRenderer> ().color = Grid.grid [a , i + 1].GetComponent<SpriteRenderer> ().color;
+				Grid.grid [a, i + 1].GetComponent<SpriteRenderer> ().color = work;
+			}*/
+			Direction (a, b + 1);
+		}
+		if (b > 0 && Fon == Grid.grid [a, b - 1].GetComponent<SpriteRenderer> ().color) {
+			Grid.grid [a, b - 1].GetComponent<SpriteRenderer> ().color = Color.clear;
+
+			/*for (int i = b - 1; i < 19; i++) {
+				work = Grid.grid [a, i].GetComponent<SpriteRenderer> ().color;
+				Grid.grid [a , i].GetComponent<SpriteRenderer> ().color = Grid.grid [a , i + 1].GetComponent<SpriteRenderer> ().color;
+				Grid.grid [a, i + 1].GetComponent<SpriteRenderer> ().color = work;
+			}*/
+			Direction (a, b - 1);
+		}
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 20; j++) {
+				if (Grid.grid [a , j].GetComponent<SpriteRenderer> ().color == Color.clear) {
+					for (int c = j; c < 19; c++) {
+						work = Grid.grid [a, c].GetComponent<SpriteRenderer> ().color;
+						Grid.grid [a , c].GetComponent<SpriteRenderer> ().color = Grid.grid [a , c + 1].GetComponent<SpriteRenderer> ().color;
+						Grid.grid [a, c + 1].GetComponent<SpriteRenderer> ().color = work;
+					}
+				}
+			}
+		}
+	}
 
 
-	void OnMouseDown (){
+		
+	void OnMouseDown () {
 
 		string s = gameObject.name;
 		int x = int.Parse (s.Substring (6, 2));
 		int y = int.Parse (s.Substring (9, 2));
-		Color work;
-		
-		Debug.Log ("x = " + x + "; y = " + y + "\n" +
+		Fon = Grid.grid [x, y].GetComponent<SpriteRenderer> ().color;
+		/*Debug.Log ("x = " + x + "; y = " + y + "\n" +
 			"(x+1) = " + (x + 1) + "; (y+1) = " + (y + 1) +
-			"|| (x-1) = " + (x - 1) + "; (y-1) = " + (y - 1));
-		int i = x;
-		int j = y;
-		int gridCountXplus = 1;
-		int gridCountXminus = 1;
-		int gridCountY = 1;
-
-
-			
-		if ((Grid.grid [x, y].GetComponent<SpriteRenderer> ().color == Grid.grid [x, y + 1].GetComponent<SpriteRenderer> ().color) |
-			(Grid.grid [x, y].GetComponent<SpriteRenderer> ().color == Grid.grid [x, y - 1].GetComponent<SpriteRenderer> ().color) |
-			(Grid.grid [x, y].GetComponent<SpriteRenderer> ().color == Grid.grid [x + 1, y].GetComponent<SpriteRenderer> ().color) |
-			(Grid.grid [x, y].GetComponent<SpriteRenderer> ().color == Grid.grid [x - 1, y].GetComponent<SpriteRenderer> ().color)) {
-
-
-				while (Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[x, j + 1].GetComponent<SpriteRenderer> ().color) {
-					Grid.grid [x, j + 1].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
-					j++;
-					gridCountY += 1;
-				} 
-
-
-			j = y;
-
-			while (Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[x, j - 1].GetComponent<SpriteRenderer> ().color) {				
-				Grid.grid [x, j - 1].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
-				j--;
-				gridCountY += 1;
-			} 
-			while (Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[i + 1, y].GetComponent<SpriteRenderer> ().color) {					
-				Grid.grid [i + 1, y].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
-				i++;
-				gridCountXplus += 1;
-			}
-			i = x;
-
-			while (Grid.grid[x,y].GetComponent<SpriteRenderer> ().color == Grid.grid[i - 1, y].GetComponent<SpriteRenderer> ().color) {					
-				Grid.grid [i - 1, y].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
-				i--;
-				gridCountXminus += 1;
-			}
-
-			Grid.grid [x, y].GetComponent<SpriteRenderer> ().color = new Color (0f, 0f, 0f, 0.255f);
-
-			for (int c = x + 1 ; c < gridCountXplus; c++) {
-				
-				for (int a = y; a < 19; a++) {
-					work = Grid.grid [c, a].GetComponent<SpriteRenderer> ().color;
-					Grid.grid [c, a].GetComponent<SpriteRenderer> ().color = Grid.grid [c, a + 1].GetComponent<SpriteRenderer> ().color;
-					Grid.grid [c, a + 1].GetComponent<SpriteRenderer> ().color = work;
-				}
-			}
-			
-			for (int n = x - 1 ; n > gridCountXminus; n--) {
-				
-				for (int m = y; m < 19; m++) {
-					work = Grid.grid [n, m].GetComponent<SpriteRenderer> ().color;
-					Grid.grid [n, m].GetComponent<SpriteRenderer> ().color = Grid.grid [n, m + 1].GetComponent<SpriteRenderer> ().color;
-					Grid.grid [n, m + 1].GetComponent<SpriteRenderer> ().color = work;
-				}
-			}
-
-			for (int q = 0; q < gridCountY; q++) {
-			
-				for (int b = j; b < 19; b++) {
-					work = Grid.grid [x, b].GetComponent<SpriteRenderer> ().color;
-					Grid.grid [x, b].GetComponent<SpriteRenderer> ().color = Grid.grid [x, b + 1].GetComponent<SpriteRenderer> ().color;
-					Grid.grid [x, b + 1].GetComponent<SpriteRenderer> ().color = work;
-				}
-			}
-
-					
-				
-
-		}
-	}
+			"|| (x-1) = " + (x - 1) + "; (y-1) = " + (y - 1));*/
 	
+		Direction (x, y);
+	}
+
+		
 	// Use this for initialization
 	void Start () {
+
 		int colorRand = Random.Range (0, 5);
 		GetComponent<SpriteRenderer> ().color = colorArr[colorRand];
 		
